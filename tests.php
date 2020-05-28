b7eefc5b8a3b79faf77edf2a070bc91e785b9975
BorisKa11 <bkazakov@mail.ru>
2020-05-28 05:00:21 +0300

    add small tests

---
+++ tests.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Tests extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,17 +3,59 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Section;
+use App\User;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Testing redirect on sections page without authorization.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testSectionsNoAuth302()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/sections');
+        $response->assertRedirect('/login');
+    }
+    
+    /**
+     * Testing an http answer on sections page with authorization.
+     *
+     * @return void
+     */
+    public function testSectionsAuth200()
+    {
+        $this->loginWithFakeUser();
+        $response = $this->get('/sections');
+        $response->assertStatus(200);
+    }
+    
+    /**
+     * Testing an http answer on sections edit page.
+     *
+     * @return void
+     */
+    public function testEditSection200()
+    {
+        $this->loginWithFakeUser();
+        $section = Section::first();
+        $response = $this->get('/sections/edit/' . $section->id);
+        $response->assertStatus(200);
+    }
+    
+    /**
+     * Adding user for application
+     *
+     * @return void
+     */
+    public function loginWithFakeUser()
+    {
+        $user = new User([
+            'id' => 1,
+            'name' => 'fake name'
+        ]);
+
+        $this->be($user);
     }
 }
